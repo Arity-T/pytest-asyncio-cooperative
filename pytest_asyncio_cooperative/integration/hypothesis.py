56ccfcedd7fab42fcbd72cc66f492735f16d54c5
Arity-T <tish.mainmail@gmail.com>
2026-01-30 23:00:55 +0300

    Agent: implement issue #32

--- pytest_asyncio_cooperative/integration/hypothesis.py
+++ pytest_asyncio_cooperative/integration/hypothesis.py
@@ -18,25 +18,13 @@ async def hypothesis_test_wrapper(item):
     default_loop = asyncio.get_running_loop()
     inner_test = item.function.hypothesis.inner_test
 
-    def async_to_sync(*args, **kwargs):
-        # FIXME: can we cache this loop across multiple runs?
-        loop = asyncio.new_event_loop()
-        task = inner_test(*args, **kwargs)
-        try:
-            loop.run_until_complete(task)
-        finally:
-            loop.close()
-
-    # Run test
-    item.function.hypothesis.inner_test = async_to_sync
-    wrapped_func_with_fixtures = functools.partial(item.function, *fixture_values)
-    await default_loop.run_in_executor(None, wrapped_func_with_fixtures)
-
-    # Do teardowns
-    item.start_teardown = time.time()
-    for teardown in teardowns:
-        try:
-            await teardown.__anext__()
-        except StopAsyncIteration:
+def async_to_sync(inner_test, *args, **kwargs):
+    # FIXME: can we cache this loop across multiple runs?
+    loop = asyncio.get_event_loop()  # Use current loop instead of creating a new one
+    task = inner_test(*args, **kwargs)
+    try:
+        return loop.run_until_complete(task)
+    finally:
+        pass  # No need to close the loop as we're using the current one
             pass
     item.stop_teardown = time.time()
