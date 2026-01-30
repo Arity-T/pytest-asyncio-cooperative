9bacd43593ab063ff82312b9cef0b9c94176b56f
root <root@6bc3d24a952c>
2026-01-30 20:05:56 +0000

    Agent: implement issue #34

--- pytest_asyncio_cooperative/integration/hypothesis.py
+++ pytest_asyncio_cooperative/integration/hypothesis.py
@@ -19,6 +19,7 @@ async def hypothesis_test_wrapper(item):
     inner_test = item.function.hypothesis.inner_test
 
     def async_to_sync(*args, **kwargs):
+        """Convert an asynchronous function into a synchronous one."
         # FIXME: can we cache this loop across multiple runs?
         loop = asyncio.new_event_loop()
         task = inner_test(*args, **kwargs)
@@ -26,17 +27,5 @@ async def hypothesis_test_wrapper(item):
             loop.run_until_complete(task)
         finally:
             loop.close()
-
-    # Run test
-    item.function.hypothesis.inner_test = async_to_sync
-    wrapped_func_with_fixtures = functools.partial(item.function, *fixture_values)
-    await default_loop.run_in_executor(None, wrapped_func_with_fixtures)
-
-    # Do teardowns
-    item.start_teardown = time.time()
-    for teardown in teardowns:
-        try:
-            await teardown.__anext__()
-        except StopAsyncIteration:
             pass
     item.stop_teardown = time.time()
